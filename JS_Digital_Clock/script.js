889c25a6ed1a0cafc57f873d69e20892c09515ef
rutikkpatel <rutikkpatel@gmail.com>
2023-02-13 23:52:30 +0530

    format code, remove unnecessary css, add bootstrap class

--- JS_Digital_Clock/script.js
+++ JS_Digital_Clock/script.js
@@ -4,12 +4,15 @@ let date;
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 setInterval(() => {
-    a = new Date();
-    // Hours Minutes and Second Concate
-    time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
-    // date = a.toLocaleDateString();
-    // Display month day in string format
-    date = a.toLocaleDateString(undefined, options);
-    document.getElementById('time').innerText = time;
-    document.getElementById('date').innerText = date;
+  a = new Date();
+  // Hours Minutes and Second Concate
+  time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+  document.getElementById('time').innerText = time;
+
+  // 20/12/2000 - Format
+  // date = a.toLocaleDateString(); 
+
+  // Display month day in string format - Wednesday, 20 December 2000
+  date = a.toLocaleDateString(undefined, options);
+  document.getElementById('date').innerText = date;
 }, 1000);
